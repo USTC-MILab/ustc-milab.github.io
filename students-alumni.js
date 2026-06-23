46591086ef554554b6ac3cf0b699766339c5e961
TiaoTiao <wzhai056@mail.ustc.edu.cn>
2026-06-23 10:33:21 +0800

    Update students-alumni.js

--- students-alumni.js
+++ students-alumni.js
@@ -1,35 +1,231 @@
 // Add or edit alumni information here.
 window.MEILAB_ALUMNI = [
     {
-        "name": "张三",
-        "destination": "中国科学技术大学博士后"
+        "name": "王堃宇（2026届博士）",
+        "destination": "斯坦福大学-博士后"
     },
     {
-        "name": "李四",
-        "destination": "华为技术有限公司"
+        "name": "朱佳莹（2026届博士）",
+        "destination": "阿里淘天"
     },
     {
-        "name": "王五",
-        "destination": "腾讯 AI Lab"
+        "name": "葛成杰（2026届博士）",
+        "destination": "清华大学-博士后"
     },
     {
-        "name": "赵六",
-        "destination": "阿里巴巴达摩院"
+        "name": "韩晗（2026届博士）",
+        "destination": "华为"
     },
     {
-        "name": "陈七",
+        "name": "杨健（2026届博士）",
+        "destination": "自变量机器人"
+    },
+    {
+        "name": "陈进泽（2026届博士）",
         "destination": "字节跳动"
     },
     {
-        "name": "刘八",
-        "destination": "清华大学博士后"
+        "name": "刘翠宇（2026届硕士）",
+        "destination": "美团"
     },
     {
-        "name": "周九",
-        "destination": "中国科学院自动化研究所"
+        "name": "王众（2026届硕士）",
+        "destination": "大连市选调生"
     },
     {
-        "name": "吴十",
-        "destination": "上海人工智能实验室"
+        "name": "谢璟毅（2026届博士）",
+        "destination": "钛动科技"
+    },
+    {
+        "name": "黄志鹏（2025届博士）",
+        "destination": "微信"
+    },
+    {
+        "name": "李尧（2025届博士）",
+        "destination": "贵州省黔南州人民政府办公室"
+    },
+    {
+        "name": "李东（2025届博士）",
+        "destination": "腾讯"
+    },
+    {
+        "name": "汪曦（2025届博士）",
+        "destination": "华为"
+    },
+    {
+        "name": "万增余（2025届博士）",
+        "destination": "华为"
+    },
+    {
+        "name": "肖杰（2025届博士）",
+        "destination": "阿里巴巴达摩院-阿里星"
+    },
+    {
+        "name": "吴蔚（2025届博士）",
+        "destination": "蚂蚁集团-蚂蚁星"
+    },
+    {
+        "name": "谭赣超（2025届博士）",
+        "destination": "华为"
+    },
+    {
+        "name": "童浩（2025届硕士）",
+        "destination": "阿里巴巴"
+    },
+    {
+        "name": "朱禹睿（2024届博士）",
+        "destination": "中兴通讯"
+    },
+    {
+        "name": "朱阳春（2024届博士）",
+        "destination": "安徽省产品质量监督检验研究院"
+    },
+    {
+        "name": "史雅雅（2024届博士）",
+        "destination": "快手公司"
+    },
+    {
+        "name": "郭鑫（2024届博士）",
+        "destination": "河南省发改委"
+    },
+    {
+        "name": "时格格（2024届硕士）",
+        "destination": "阿里淘天"
+    },
+    {
+        "name": "曹成志（2024届硕士）",
+        "destination": "卡耐基梅隆大学-读博"
+    },
+    {
+        "name": "甘文君（2024届硕士）",
+        "destination": "科大讯飞"
+    },
+    {
+        "name": "黄宇坤（2023届博士）",
+        "destination": "香港大学-博士后"
+    },
+    {
+        "name": "胡冰玉（2023届博士）",
+        "destination": "杭州电子科技大学"
+    },
+    {
+        "name": "董林滔（2023届博士）",
+        "destination": "中电28所"
+    },
+    {
+        "name": "朱凯（2023届博士）",
+        "destination": "阿里巴巴达摩院-阿里星"
+    },
+    {
+        "name": "冯睿蠡（2023届博士）",
+        "destination": "Nvidia"
+    },
+    {
+        "name": "赵宇澄（2023届博士）",
+        "destination": "原力灵机"
+    },
+    {
+        "name": "吴思颖（2023届博士）",
+        "destination": "合肥人工智能研究院"
+    },
+    {
+        "name": "汪昊典（2023届硕士）",
+        "destination": "国能数智科技开发有限公司"
+    },
+    {
+        "name": "杨子禾（2023届硕士）",
+        "destination": "中国电信"
+    },
+    {
+        "name": "郑贺亮（2022届博士）",
+        "destination": "数美万物"
+    },
+    {
+        "name": "翟伟（2022届博士）",
+        "destination": "中国科学技术大学-特任副研究员"
+    },
+    {
+        "name": "郑可成（2022届博士）",
+        "destination": "蚂蚁集团"
+    },
+    {
+        "name": "王昊（2022届博士）",
+        "destination": "华为"
+    },
+    {
+        "name": "康昇（2022届硕士）",
+        "destination": "字节跳动"
+    },
+    {
+        "name": "赵瑞静（2022届硕士）",
+        "destination": "天翼云"
+    },
+    {
+        "name": "王洋（2021届博士）",
+        "destination": "长安大学-副教授"
+    },
+    {
+        "name": "刘大庆（2021届博士）",
+        "destination": "京东-管培生"
+    },
+    {
+        "name": "朱勰戎（2021届硕士）",
+        "destination": "招商银行"
+    },
+    {
+        "name": "吴昊泽（2021届硕士）",
+        "destination": "字节跳动"
+    },
+    {
+        "name": "叶鲁建（2021届硕士）",
+        "destination": "宇视科技"
+    },
+    {
+        "name": "谢言威（2021届硕士）",
+        "destination": "保密"
+    },
+    {
+        "name": "李政青（2020届硕士）",
+        "destination": "OPPO"
+    },
+    {
+        "name": "杨天昊（2020届硕士）",
+        "destination": "江苏省南通市发改委"
+    },
+    {
+        "name": "马圣杰（2020届硕士）",
+        "destination": "上海诚奇私募基金有限公司"
+    },
+    {
+        "name": "赵瑞（2020届硕士）",
+        "destination": "华为"
+    },
+    {
+        "name": "刘嘉威（2019届博士）",
+        "destination": "中国科学技术大学-副教授"
+    },
+    {
+        "name": "王冲（2019届博士）",
+        "destination": "中国科学技术大学-特任副研究员"
+    },
+    {
+        "name": "郑晓菊（2019届博士）",
+        "destination": "上海浦东发展银行"
+    },
+    {
+        "name": "温忻（2019届硕士）",
+        "destination": "华为"
+    },
+    {
+        "name": "陈迪（2019届硕士）",
+        "destination": "华为"
+    },
+    {
+        "name": "孙亮（2019届硕士）",
+        "destination": "合肥是也科技有限公司"
+    },
+    {
+        "name": "吴乾坤（2018届硕士）",
+        "destination": "中电28所"
     }
 ];
